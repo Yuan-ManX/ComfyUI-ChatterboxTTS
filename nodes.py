bb780cdbbff9c5fd15af7f21ca90560bee79f95a
Yuan-Man <68322456+Yuan-ManX@users.noreply.github.com>
2025-05-30 11:30:25 +0800

    Update nodes.py

--- nodes.py
+++ nodes.py
@@ -6,6 +6,14 @@ import torchaudio as ta
 from chatterbox.tts import ChatterboxTTS
 
 
+def set_seed(seed: int):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    random.seed(seed)
+    np.random.seed(seed)
+
+
 class LoadChatterboxTTSModel:
     @classmethod
     def INPUT_TYPES(s):
@@ -46,3 +54,65 @@ class ChatterboxTTSPrompt:
     def input_text(self, text):
         prompt = text
         return (prompt,)
+
+
+class ChatterboxTTS:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "model": ("MODEL",),
+                "prompt": ("PROMPT",),
+                "exaggeration": (
+                    "FLOAT",
+                    {
+                        "default": 0.5,
+                        "min": 0.25,
+                        "max": 2,
+                        "step": 0.05,
+                    },
+                ),
+                "temperature": (
+                    "FLOAT",
+                    {
+                        "default": 0.8,
+                        "min": 0.05,
+                        "max": 5,
+                        "step": 0.05,
+                    },
+                ),
+                ""seed_num": ("INT", {"default": 0}),
+                "cfgw": (
+                    "FLOAT",
+                    {
+                        "default": 0.5,
+                        "min": 0.2,
+                        "max": 1,
+                        "step": 0.05,
+                    },
+                ),
+            }
+        }
+
+    RETURN_TYPES = ("TTS", "SR",)
+    RETURN_NAMES = ("tts", "sr",)
+    FUNCTION = "generate"
+    CATEGORY = "ChatterboxTTS"
+
+    def generate(self, model, prompt, exaggeration, temperature, seed_num, cfgw):
+
+        if model is None:
+            model = ChatterboxTTS.from_pretrained(device="cuda")
+    
+        if seed_num != 0:
+            set_seed(int(seed_num))
+    
+        wav = model.generate(
+            prompt,
+            exaggeration=exaggeration,
+            temperature=temperature,
+            cfg_weight=cfgw,
+        )
+    
+        return (wav, model.sr)
+
