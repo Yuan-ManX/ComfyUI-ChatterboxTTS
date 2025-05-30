693b4619f07c2b8d212041e8bfc420a5a1499524
Yuan-Man <68322456+Yuan-ManX@users.noreply.github.com>
2025-05-30 12:15:25 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,27 @@
+from .nodes import LoadChatterboxTTSModel, LoadChatterboxVCModel, ChatterboxPrompt, ChatterboxAudioPrompt, ChatterboxTTS, LoadChatterboxAudio, LoadChatterboxTargetAudio, ChatterboxVC, SaveChatterboxAudio
 
+NODE_CLASS_MAPPINGS = {
+    "LoadChatterboxTTSModel": LoadChatterboxTTSModel,
+    "LoadChatterboxVCModel": LoadChatterboxVCModel,
+    "ChatterboxPrompt": ChatterboxPrompt,
+    "ChatterboxAudioPrompt": ChatterboxAudioPrompt,
+    "ChatterboxTTS": ChatterboxTTS,
+    "LoadChatterboxAudio": LoadChatterboxAudio,
+    "LoadChatterboxTargetAudio": LoadChatterboxTargetAudio,
+    "ChatterboxVC": ChatterboxVC,
+    "SaveChatterboxAudio": SaveChatterboxAudio,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "LoadChatterboxTTSModel": "Load Chatterbox TTS Model",
+    "LoadChatterboxVCModel": "Load Chatterbox VC Model",
+    "ChatterboxPrompt": "Chatterbox Prompt",
+    "ChatterboxAudioPrompt": "Chatterbox Audio Prompt",
+    "ChatterboxTTS": Chatterbox TTS",
+    "LoadChatterboxAudio": "Load Chatterbox Audio",
+    "LoadChatterboxTargetAudio": "Load Chatterbox Target Audio",
+    "ChatterboxVC": "Chatterbox VC",
+    "SaveChatterboxAudio": "Save Chatterbox Audio",
+} 
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
