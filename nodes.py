ed30b70a35c381d076f4d7f8fe95e27ec6290fdf
Yuan-Man <68322456+Yuan-ManX@users.noreply.github.com>
2025-05-30 11:11:06 +0800

    Update nodes.py

--- nodes.py
+++ nodes.py
@@ -1,1 +1,24 @@
+import torchaudio as ta
+from chatterbox.tts import ChatterboxTTS
 
+
+class ChatterboxTTSPrompt:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "text": ("STRING", {
+                    "default": "Ezreal and Jinx teamed up with Ahri, Yasuo, and Teemo to take down the enemy's Nexus in an epic late-game pentakill.",
+                    "multiline": True
+                }),
+            }
+        }
+
+    RETURN_TYPES = ("PROMPT",)
+    RETURN_NAMES = ("prompt",)
+    FUNCTION = "input_text"
+    CATEGORY = "ChatterboxTTS"
+
+    def input_text(self, text):
+        prompt = text
+        return (prompt,)
